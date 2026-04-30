6c1f92254a65e6b21b9bf304f198a42ae4a73b4a
RIZK <rizkalhamdan@gmail.com>
2026-04-30 15:36:32 +0300

    First copy .. v:1.0 .. test it now

---
+++ core/system.js
@@ -0,0 +1,47 @@
+import { Ident } from "/core/ident.js";
+
+class System {
+	
+	constructor() {
+		
+		if(Ident.is_append()) {
+			if(this.is_exist() ) { }
+
+			else this.reset()
+		} else {
+			// await sleep(800)
+			location.replace("/")
+		}
+		
+	}
+
+	is_exist() {
+		return localStorage.miniatmbalance && localStorage.miniatmlogs
+	}
+
+	reset() {
+		var initial_balance = "0",
+			initial_data = [{type: "New account", date: new Date().getTime(), status: true, id: 0}]
+		
+		localStorage.miniatmbalance = initial_balance
+		localStorage.miniatmlogs = JSON.stringify(initial_data)
+
+		return true
+	}
+
+	append(q = {type: "Unknown input", id: 0, date: new Date().getTime(), status: false}) {
+		var ol = JSON.parse(localStorage.miniatmlogs)
+		ol.push(q)
+
+		localStorage.miniatmlogs = JSON.stringify(od)
+
+		return true
+	}
+
+	get_logs() {
+		if(! this.is_exist()) return []
+		return JSON.parse(localStorage.miniatmlogs)
+	}
+}
+
+export class system extends System {}
